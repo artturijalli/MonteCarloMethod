6531fd9feb47e220b48adaf86c1a4519518daef5
artturijalli <artturi.jalli@aalto.fi>
2020-10-07 21:25:29 +0300

    Refactor main function

--- Monte_carlo_simulation_project/main.cpp
+++ Monte_carlo_simulation_project/main.cpp
@@ -4,29 +4,20 @@
 //  Copyright © 2020 Artturi Jalli. All rights reserved.
 
 #include <iostream>
-#include <string>
-#include <math.h>
-#include <vector>
-#include <numeric>
 
+#include "global_params.hpp"
 #include "input_reader.hpp"
-#include "statistical_analysis_methods.hpp"
-#include "runtime_clock.hpp"
+
 #include "buffons_needle.hpp"
 #include "monte_carlo_pi_approximation_circle.hpp"
-#include "global_params.hpp"
 
-using namespace std;
- 
-int main()
-{
+void round_1_assignment(){
     read_params();
-    
     printf("M = %d \nN = %d \nIn Buffon's needle, the line width is %f \nNeedle length is %f \n\n", M, N, D_LINES, L_NEEDLE);
-    
     stats_pi_approximation();
     stats_buffons_needle();
-    
-    getchar();
-    return 0;
+}
+ 
+int main() {
+    round_1_assignment();
 }
